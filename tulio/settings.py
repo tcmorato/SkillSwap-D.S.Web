9eb3141e0a1837bccd9af238b6910501a90d9d00
tuliomorato <tcmorato28@gmail.com>
2025-09-20 19:06:36 -0300

    parte 1

--- tulio/settings.py
+++ tulio/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'site_tulio',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
